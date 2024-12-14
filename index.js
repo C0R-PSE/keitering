a94d6b9ba2636aed1f025d17da1da111cce181e9
CORPSE <d.b.17f@mail.ru>
2024-12-14 04:17:02 +0300

    minor changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const menu_button_top = document.querySelector('.menu_button_top')
 const menu = document.querySelector('.menu_top')
-const tabs = [...document.querySelector('.navigation').children]
 const content_grid = document.querySelector('.content_grid')
 const hookahs = content_grid.querySelector('.hookahs')
 const info_sheet = document.querySelector('.info_sheet')
+const navigation = content_grid.querySelector('.navigation')
+const tabs = [...navigation.children]
 
 const data = await fetch('./data.json').then(resp => resp.json())
-console.log(data)
+//console.log(data)
 
 function toggle_menu() {
     menu.classList.toggle('active')
@@ -17,7 +18,13 @@ function hide_menu() {
     menu.style.bottom = ""
 }
 function open_tab(tab){
-    content_grid.setAttribute('active', tab.getAttribute('tag'))
+    const currently_active = [...content_grid.getElementsByClassName('active')]
+    for (var i in currently_active) {
+        console.log(currently_active[i])
+        currently_active[i].classList.remove('active')
+    }
+    navigation.querySelector('[tag="' + tab + '"]').classList.add("active")
+    content_grid.querySelector('.content').querySelector('[tag="' + tab + '"]').classList.add("active")
 }
 function checkGridWidth() {
     const contentWidth = Math.min(window.innerWidth - 100, 1600) - 325
@@ -35,7 +42,6 @@ function show_info(hookah) {
 }
 
 for (var i in data.hookahs) {
-    console.log(data.hookahs[i])
     const hookah_card = document.createElement('div')
     hookah_card.classList.add('hookah_card')
     hookah_card.setAttribute('hookahId', i)
@@ -53,7 +59,7 @@ for (var i in data.hookahs) {
 menu_button_top.addEventListener('click', () => {toggle_menu()})
 window.addEventListener('scroll', () => {hide_menu()})
 for (var i in tabs) {
-    tabs[i].addEventListener('click', (e) => {open_tab(e.target)})
+    tabs[i].addEventListener('click', (e) => {open_tab(e.target.getAttribute('tag'))})
 }
 checkGridWidth()
 window.addEventListener('resize', () => {checkGridWidth()})
