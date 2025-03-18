cbf83f10c78e1d61a80a6b20fd990dc8c408da02
CORPSE <d.b.17f@mail.ru>
2025-03-18 20:18:22 +0300

    -

---
+++ src/Components/Hookah_Card.jsx
@@ -0,0 +1,14 @@
+function Hookah_Card() {
+    return(
+        <div className="hookah_card">
+            <div className="buttons_sheet">
+                <div className="view_button"></div>
+                <div className="edit_button"></div>
+            </div>
+            <div className="card_footer">
+                <div className="label"></div>
+            </div>
+        </div>
+    )
+}
+export default Hookah_Card
